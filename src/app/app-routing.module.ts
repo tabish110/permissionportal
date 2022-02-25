b38b41f8ba7dbb44c4c690e40a7c051f1f157730
muhammad <muhammad.42029@iqra.edu.pk>
2022-02-25 13:19:27 +0500

    declare the path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TableComponent } from './table/table.component';
 import { LoginComponent } from './login/login.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'table', component: TableComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'top', component: TopbarComponent }
 
 
 ];
